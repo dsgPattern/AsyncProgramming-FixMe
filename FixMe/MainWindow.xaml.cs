ae3576c1d16e2aec03aeef9d0bc9325b4c25c5e6
Sirb Daniel <daniel.sirb@accesa.eu>
2019-08-23 09:27:39 +0300

    Simulation of long processing blocking the UI thread

---
+++ FixMe/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FixMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ((Storyboard)FindResource("WaitStoryboard")).Begin();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            txtStatus.Text = string.Empty;
+            var sw = Stopwatch.StartNew();
+            while (true)
+            {
+                if (sw.ElapsedMilliseconds > 5000)
+                    break;
+            }
+
+            txtStatus.Text = $"Finished stupid while loop";
+        }
+    }
+}
